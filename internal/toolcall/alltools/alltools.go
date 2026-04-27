33534671cc33e9d15c2b81cf7574217d1c632317
Nan Jun Jie <nanjunjie@139.com>
2026-04-28 01:44:12 +0800

    refactor: 删除 Git 工具
    Git 工具作为 shell 的薄封装，参数传递不可靠（-A 被误解析），
    且其 SubCommands() 枚举数百个子命令浪费大量上下文窗口。
    shell 工具已原生支持 git 命令，删掉可省 token。

--- internal/toolcall/alltools/alltools.go
+++ internal/toolcall/alltools/alltools.go
@@ -6,7 +6,6 @@ import (
 	_ "gitcode.com/dscli/dscli/internal/toolcall/ask"
 	_ "gitcode.com/dscli/dscli/internal/toolcall/code"
 	_ "gitcode.com/dscli/dscli/internal/toolcall/file"
-	_ "gitcode.com/dscli/dscli/internal/toolcall/git"
 	_ "gitcode.com/dscli/dscli/internal/toolcall/issue"
 	_ "gitcode.com/dscli/dscli/internal/toolcall/make"
 	_ "gitcode.com/dscli/dscli/internal/toolcall/shell"
